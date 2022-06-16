a61842277de6d2dc62db1c3fa25757fe59be8937
Ben Keddie <ben@blanket.com>
2022-06-15 17:30:08 -0700

    first commit

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Details for Coach
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Filter
+    </div>
+    <div>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach</router-link>
+        </div>
+        List of Coaches
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
